b9800ba8bb78ff6d4b3a2b6afec77ea0e0ca31fb
Dasporal <dasporal@gmail.com>
2016-12-12 11:17:55 +0100

    c'est moche

--- src/AppBundle/Controller/LectureCtrlController.php
+++ src/AppBundle/Controller/LectureCtrlController.php
@@ -10,11 +10,12 @@ use Symfony\Component\HttpFoundation\Response;
 
 class LectureCtrlController extends Controller
 {
+
     /**
-     * @Route(path="/index/",
+     * @Route(path="/lectureCtrl/",
      *     name="lectureCtrl_index")
      */
-    public function indexAction()
+    public function lectureCtrlAction()
     {
         $employe = new Employe(1, "Dupont","Jean", new \DateTime('1988-12-25'));
         $formulaire = $this->createFormBuilder($employe)
@@ -24,14 +25,14 @@ class LectureCtrlController extends Controller
             ->add('dateNaissance','date')
             ->add('Enregistrer','submit')
             ->getForm();
-        return $this->render('AppBundle:LectureCtrl:index.html.twig',array('leFormulaire'=>$formulaire->createView()));
+        return $this->render('AppBundle:LectureCtrl:lectureCtrl.html.twig',array('leFormulaire'=>$formulaire->createView()));
     }
 
     /**
      * @Route(path="/afficheEmpParam/",
      *     name="lectureCtrl_afficheEmpParam")
      */
-    public function afficheEmpParamAction($formulaire)
+    public function afficheEmpParamAction()
     {
         $employe = new Employe(1, "Dupont","Jean", new \DateTime('1988-12-25'));
         $formulaire = $this->createFormBuilder($employe)
@@ -43,6 +44,32 @@ class LectureCtrlController extends Controller
             ->getForm();
         $sport='Rugby';
         $autos=array('Ferrari','Aston Martin', 'Porsche');
-        return $this->render('AppBundle:LectureCtrl:afficheEmpParam.html.twig',array('leFormulaire'=>$formulaire->createView(), 'sport'=>$sport,'autos'=>$autos));
+        return $this->render('AppBundle:LectureCtrl:afficheEmpParam.html.twig',array('leFormulaire'=>$formulaire->createView(),
+            'sport'=>$sport,
+            'autos'=>$autos));
+    }
+
+    /**
+     * @Route(path="/afficheEmpTwig/",
+     *     name="lectureCtrl_afficheEmpTwig")
+     */
+    public function afficheEmpTwigAction()
+    {
+        $request=$this->get('request');
+        if ($request->getMethod()==='POST'){
+            $a=0;
+        }
+        $employe = new Employe(1, "Dupont","Jean", new \DateTime('1988-12-25'));
+        $employe->setPrenom("Jean");
+        $employe->getDateNaissance(new \DateTime('today'));
+        $formulaire = $this->createFormBuilder($employe)
+            ->add('num','number')
+            ->add('nom','text')
+            ->add('prenom','text')
+            ->add('dateNaissance','date')
+            ->add('Enregistrer','submit')
+            ->getForm();
+        return $this->render('AppBundle:LectureCtrl:afficheEmpTwig.html.twig',
+            array('uneEntree'=>'je viens du controller','leFormulaire'=>$formulaire->createView()));
     }
 }
